d3641526eee6eb4939871e592b4301e113370389
SlavaTurovski <turowscky2018@gmail.com>
2025-07-31 08:29:23 +0300

    правки после ревью

--- src/main/java/ru/practicum/shareit/user/dal/UserMemoryStorage.java
+++ src/main/java/ru/practicum/shareit/user/dal/UserMemoryStorage.java
@@ -8,20 +8,31 @@ import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 @Slf4j
 @Repository
 public class UserMemoryStorage implements UserStorage {
     private final Map<Long, User> users = new HashMap<>();
-    private final Map<String, Long> emailToId = new HashMap<>();
+    private final Set<String> emails = new HashSet<>();
     private Long counterId = 0L;
 
+    private User findUserByEmail(String email) {
+        return users.values().stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
+    }
+
     public void validateEmail(User newUser) {
-        Long existingUserId = emailToId.get(newUser.getEmail());
-        if (existingUserId != null && !existingUserId.equals(newUser.getId())) {
-            throw new IllegalArgumentException("Пользователь с таким email уже существует!");
+        if (emails.contains(newUser.getEmail())) {
+            User existingUser = findUserByEmail(newUser.getEmail());
+            if (existingUser != null && !existingUser.getId().equals(newUser.getId())) {
+                throw new IllegalArgumentException("Пользователь с таким email уже существует!");
+            }
         }
     }
 
@@ -32,7 +43,7 @@ public class UserMemoryStorage implements UserStorage {
             validateEmail(user);
             user.setId(++counterId);
             users.put(user.getId(), user);
-            emailToId.put(user.getEmail(), user.getId());
+            emails.add(user.getEmail());
             log.info("Пользователь создан [{}], присвоен id [{}]", user, user.getId());
             return user;
         } catch (InternalServerException ex) {
@@ -46,18 +57,16 @@ public class UserMemoryStorage implements UserStorage {
         if (user.getId() == null) {
             throw new ValidationException("Для обновления пользователя необходимо указать id!");
         }
-        User existingUser = users.get(user.getId());
-        if (existingUser == null) {
-            throw new NotFoundException("Пользователь с id = " + user.getId() + " не найден!");
+        User existingUser = getById(user.getId());
+        if (!existingUser.getEmail().equals(user.getEmail())) {
+            if (emails.contains(user.getEmail())) {
+                throw new IllegalArgumentException("Пользователь с таким email уже существует!");
+            }
+            emails.remove(existingUser.getEmail());
+            emails.add(user.getEmail());
         }
-        String oldEmail = existingUser.getEmail();
-        String newEmail = user.getEmail();
         validateEmail(user);
         users.put(user.getId(), user);
-        if (!oldEmail.equals(newEmail)) {
-            emailToId.remove(oldEmail);
-            emailToId.put(newEmail, user.getId());
-        }
         log.info("Пользователь с id [{}] успешно обновлён: [{}]", user.getId(), user);
         return user;
     }
@@ -66,7 +75,7 @@ public class UserMemoryStorage implements UserStorage {
     public void delete(Long userId) {
         User removedUser = users.remove(userId);
         if (removedUser != null) {
-            emailToId.remove(removedUser.getEmail());
+            emails.remove(removedUser.getEmail()); // удаляем email из Set при удалении пользователя
         }
     }
 
