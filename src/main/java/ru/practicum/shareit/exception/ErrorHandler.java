13fe81249bef9815e1a5925417d5e335b202ff03
SlavaTurovski <turowscky2018@gmail.com>
2025-07-31 07:20:26 +0300

    правки после ревью

---
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.exception;
+
+import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        return new ErrorResponse("Ошибка валидации", e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return new ErrorResponse("Ошибка поиска", e.getMessage());
+    }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        return new ErrorResponse("Ошибка сервера", e.getMessage());
+    }
+
+}
--- src/main/java/ru/practicum/shareit/user/dal/UserMemoryStorage.java
+++ src/main/java/ru/practicum/shareit/user/dal/UserMemoryStorage.java
@@ -1,54 +1,73 @@
 package ru.practicum.shareit.user.dal;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.exception.InternalServerException;
 import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
+@Slf4j
 @Repository
 public class UserMemoryStorage implements UserStorage {
     private final Map<Long, User> users = new HashMap<>();
-    private final Set<String> emails = new HashSet<>();
+    private final Map<String, Long> emailToId = new HashMap<>();
     private Long counterId = 0L;
 
     public void validateEmail(User newUser) {
-        if (emails.contains(newUser.getEmail())) {
+        Long existingUserId = emailToId.get(newUser.getEmail());
+        if (existingUserId != null && !existingUserId.equals(newUser.getId())) {
             throw new IllegalArgumentException("Пользователь с таким email уже существует!");
         }
     }
 
     @Override
     public User add(User user) {
-        validateEmail(user);
-        user.setId(++counterId);
-        users.put(user.getId(), user);
-        emails.add(user.getEmail());
-        return user;
+        log.info("Создание нового пользователя [{}]", user);
+        try {
+            validateEmail(user);
+            user.setId(++counterId);
+            users.put(user.getId(), user);
+            emailToId.put(user.getEmail(), user.getId());
+            log.info("Пользователь создан [{}], присвоен id [{}]", user, user.getId());
+            return user;
+        } catch (InternalServerException ex) {
+            throw new RuntimeException("Не удалось создать пользователя!");
+        }
     }
 
     @Override
     public User update(User user) {
+        log.info("Обновление пользователя с id [{}], данные [{}]", user.getId(), user);
+        if (user.getId() == null) {
+            throw new ValidationException("Для обновления пользователя необходимо указать id!");
+        }
         User existingUser = users.get(user.getId());
         if (existingUser == null) {
-            throw new NotFoundException("Пользователь не найден!");
+            throw new NotFoundException("Пользователь с id = " + user.getId() + " не найден!");
         }
-        if (!existingUser.getEmail().equals(user.getEmail())) {
-            validateEmail(user);
-            emails.remove(existingUser.getEmail());
-            emails.add(user.getEmail());
-        }
+        String oldEmail = existingUser.getEmail();
+        String newEmail = user.getEmail();
+        validateEmail(user);
         users.put(user.getId(), user);
+        if (!oldEmail.equals(newEmail)) {
+            emailToId.remove(oldEmail);
+            emailToId.put(newEmail, user.getId());
+        }
+        log.info("Пользователь с id [{}] успешно обновлён: [{}]", user.getId(), user);
         return user;
     }
 
     @Override
     public void delete(Long userId) {
-        users.remove(userId);
+        User removedUser = users.remove(userId);
+        if (removedUser != null) {
+            emailToId.remove(removedUser.getEmail());
+        }
     }
 
     @Override
